d06f523653c154940a4da7063e7ec8ac76fd7639
Ryan <69523694+Rseckman@users.noreply.github.com>
2020-10-07 15:42:40 -0400

    starting file structure

---
+++ config/orm.js
@@ -0,0 +1,20 @@
+var connection = require("./connection.js");
+
+// Object Relational Mapper (ORM)
+
+// The ?? signs are for swapping out table or column names
+// The ? signs are for swapping out other values
+
+var orm = {
+  selectAll: function() {
+    
+  },
+  insertOne: function() {
+   
+  },
+  updateOne: function() {
+
+  }
+};
+
+module.exports = orm;
